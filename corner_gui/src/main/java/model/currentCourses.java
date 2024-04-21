4c4545b20b1639a1ca74af871b8d915f9b42f422
Vigneswaran Chinnasami <vigneswaran0103@gmail.com@domain.com>
2024-04-20 22:01:42 -0400

    cuurentCourses

--- corner_gui/src/main/java/model/currentCourses.java
+++ corner_gui/src/main/java/model/currentCourses.java
@@ -1,13 +1,14 @@
+// Model class for representing current courses
 package model;
 import java.util.ArrayList;
 public class currentCourses {
-    private String courseID;
-    private String grade;
-    private String semester;
-    private long year;
+    private String courseID;// Unique identifier for the course
+    private String grade;// Grade obtained in the course
+    private String semester;// Semester in which the course was taken
+    private long year;// Year in which the course was taken
 
     
-
+    // Constructor for CurrentCourses class
     public currentCourses(String courseID, String grade, String semester,long year) {
         this.courseID = courseID;
         this.grade = grade;
